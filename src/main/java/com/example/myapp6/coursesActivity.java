0bfbe18d7196cd3bc9ff6c802335740edf51ff24
alaasahraoui <tipaza.alaa@hotmail.com>
2020-05-03 01:20:50 +0100

    final2

--- src/main/java/com/example/myapp6/coursesActivity.java
+++ src/main/java/com/example/myapp6/coursesActivity.java
@@ -11,45 +11,44 @@ import android.view.View;
 import java.net.URL;
 
 public class coursesActivity extends AppCompatActivity {
-public CardView c1;
-    public CardView c2;
-    public CardView c3;
+public CardView c11;
+    public CardView c22;
+    public CardView c33;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_courses);
-        c1=findViewById(R.id.c1);
-        c2=findViewById(R.id.c2);
-        c3=findViewById(R.id.c3);
-        c1.setOnClickListener(new View.OnClickListener() {
+        c11=findViewById(R.id.c111);
+        c22=findViewById(R.id.c22);
+        c33=findViewById(R.id.c33);
+
+
+        c11.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {openlink();
 
             }
-        }
-        );
+        });
 
-        c2.setOnClickListener(new View.OnClickListener() {
-                                  @Override
-                                  public void onClick(View v) {openlink();
+        c22.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {openlink();
 
-                                  }
-                              }
-        );
+            }
+        });
+        c33.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {openlink();
+
+            }
+        });
 
 
-        c3.setOnClickListener(new View.OnClickListener() {
-                                  @Override
-                                  public void onClick(View v) {openlink();
-
-                                  }
-                              }
-        );
     }
 
 
 public void openlink(){
-    Uri uri = Uri.parse("https://drive.google.com/open?id=1YNVV8kvmpKdlklF5HjpuiRLyhr1iAJ2G"); // missing 'http://' will cause crashed
+    Uri uri = Uri.parse("https://drive.google.com/open?id=1bFyHobTpWRBQjzrOfy6GbXuvxDwzE-4Z"); // missing 'http://' will cause crashed
     Intent intent = new Intent(Intent.ACTION_VIEW, uri);
     startActivity(intent);
 
